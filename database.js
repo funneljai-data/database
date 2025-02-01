4e8985a0837054f11a2ed9cee761e6863234f953
funneljai212 <jaliyah.seal12@gmail.com>
2025-02-01 09:11:56 -0500

    Update database.js

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 // database.js
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./strings.db');
+const db = new sqlite3.Database('./fails.db');
 
 db.serialize(() => {
     db.run("CREATE TABLE IF NOT EXISTS strings (id INTEGER PRIMARY KEY AUTOINCREMENT, value TEXT)");

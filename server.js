9086a3262bf0cc165a8954ec28bc5b69a1071dc2
funneljai212 <jaliyah.seal12@gmail.com>
2025-01-31 21:54:57 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./database');
-
+const cors = require('cors')
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public')); // Serve static files from the 'public' directory
-
+app.options('*', cors()); // Enable preflight for all routes
+app.use(cors({
+    origin: 'https://funneljai-hub.github.io', // Allow only your specific origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 // Route to handle string submission
 app.post('/submit', (req, res) => {
     const { value } = req.body;
+    console.log(req.body)
     db.run("INSERT INTO strings (value) VALUES (?)", [value], function(err) {
         if (err) {
             return res.status(500).send("Error storing string.");
